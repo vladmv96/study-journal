4cb9a066f61202f52211e36266e074addcbc9f00
vladmv96 <vladikas1996@gmail.com>
2019-09-08 14:03:45 +0300

    added general components

---
+++ src/components/MainPageHeader.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles/MainPageHeader.css';
+import { STUDENT_ROLE, TEACHER_ROLE } from '../sources/constants/roles';
+import { CARDS_ROUTE, HOMEWORKS_ROUTE } from '../sources/constants/routes';
+
+function StudentMainPageHeader({
+    setCurrentPage,
+    role
+}) {
+
+    return (
+        <div className='mainPageHeader'>
+            {[STUDENT_ROLE, TEACHER_ROLE].includes(role) &&
+            <button className='headerNavButton' onClick={() => setCurrentPage(CARDS_ROUTE)}>Предметы</button>
+            }
+            {role === STUDENT_ROLE &&
+              <button className='headerNavButton'  onClick={() => setCurrentPage(HOMEWORKS_ROUTE)}>Домашние задания</button>
+            }
+            <button className='closeButton'>Выход</button>
+        </div>
+    );
+}
+
+export default StudentMainPageHeader;
--- src/components/teacher/TeacherMainPage.js
+++ src/components/teacher/TeacherMainPage.js
@@ -1,16 +1,10 @@
 import React, { useState } from 'react';
 import MainPageHeader from '../MainPageHeader';
 import CardsContainer from '../CardsContainer';
-import SubjectPage from './SubjectPage';
-import HomeworksPage from './HomeworksPage';
-import { STUDENT_ROLE } from '../../sources/constants/roles';
-import {
-    CARDS_ROUTE,
-    MAIN_ROUTE,
-    HOMEWORKS_ROUTE
-} from '../../sources/constants/routes';
+import { TEACHER_ROLE } from '../../sources/constants/roles';
+import { CARDS_ROUTE, MAIN_ROUTE } from '../../sources/constants/routes';
 
-function StudentMainPage() {
+function TeacherMainPage() {
     const [currentPage, setCurrentPage] = useState(CARDS_ROUTE);
     const [cardId, setCardId] = useState(null);
 
@@ -20,23 +14,17 @@ function StudentMainPage() {
     }
 
     return (
-        <div className='studentMainPage'>
+        <div className='TeacherMainPage'>
             <MainPageHeader
                 setCurrentPage={setCurrentPage}
-                role={STUDENT_ROLE}
+                role={TEACHER_ROLE}
             />
             {currentPage === CARDS_ROUTE &&
                 <CardsContainer
                     handleCardClick={handleCardClick}
                  />}
-            {currentPage === MAIN_ROUTE &&
-            <SubjectPage
-                cardId={cardId}
-            />}
-            {currentPage === HOMEWORKS_ROUTE &&
-            <HomeworksPage />}
         </div>
     );
 }
 
-export default StudentMainPage;
+export default TeacherMainPage;
